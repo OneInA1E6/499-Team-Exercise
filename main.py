0739f0f865f638f33b235317eefc1b48471dacca
mvallido <vallido.michael@gmail.com>
2022-09-21 12:03:32 -0700

    Added subtraction function

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ def multiplication(n1, n2):
     result = n1 * n2
     print('The result is: ', result)
    
-
 ##Adding feature for calculator
 def adding(x,y):
     return(x+y)
 
+##This function subtracts y from x :)
+def subtract(x,y):
+    return(x-y)
+ 
 ##This functions two numbers, x (the numerator) and y (the denominator),
 ##and returns the division of x by y.
 def divide(x, y):
@@ -19,7 +22,5 @@ def divide(x, y):
 
 ##This function takes in two numbers, x (the numerator) and y (the denominator),
 ##and returns the largest whole division of x by y, and the remainder
-def divideWithRemainder(x, y)
+def divideWithRemainder(x, y):
     return math.floor(x/y), x%y
-
-
