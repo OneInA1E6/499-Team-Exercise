8f028e5f98e5bef9dee860303f1fadd979789e59
Gabriel McLachlan <75510344+GMcLachlan45@users.noreply.github.com>
2022-09-21 10:33:49 -0700

    add division functionality to the calculator

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ def main():
 def adding(x,y):
     return(x+y)
 
+##This functions two numbers, x (the numerator) and y (the denominator),
+##and returns the division of x by y.
+def divide(x, y):
+    return (x/y)
+
+
 
 
